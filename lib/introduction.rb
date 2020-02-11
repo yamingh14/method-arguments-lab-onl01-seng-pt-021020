52092187572b34285b57e413d227b43e12355c63
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-11 01:13:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
 end
 
 
-def introduction_with_language(name, lanugage)
-  puts "Hi, my name is Dan and I am learning to program in Ember.js."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
